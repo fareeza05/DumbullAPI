38b8f857aa35037684e2cb67131e707b834ed892
Fareeza Rahman <fareezarahman123@gmail.com>
2023-03-26 11:14:40 -0400

    cors

--- index.js
+++ index.js
@@ -14,24 +14,24 @@ const Card = require('./models/card');
 const User = require('./models/user');
 
 
-// var allowedOrigins = process.env.allowedOrigins.split(',');
+var allowedOrigins = process.env.allowedOrigins.split(',');
 
 // using express, creating express app object
 const app  = express();
 
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         // allow requests with no origin 
-//         // (like mobile apps or curl requests)
-//         if (!origin) return callback(null, true);
-
-//        if (allowedOrigins.indexOf(origin) === -1) {
-//            var msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
-//            return callback(new Error(msg), false);
-//        }
-//        return callback(null, true);
-//    }
-// }));
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        console.log(origin)
+       if (allowedOrigins.indexOf(origin) === -1) {
+           var msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
+           return callback(new Error(msg), false);
+       }
+       return callback(null, true);
+   }
+}));
 
 //using body-parser middleware
 app.use(bodyParser.json());
